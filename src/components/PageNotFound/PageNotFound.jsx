9dc2541632cc8ca895f8a49bd393c9d48626cc39
DiNeez <dneez86@gmail.com>
2022-12-07 18:07:53 +0200

    implemented 404 page

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom'
+import { SiProbot } from 'react-icons/si'
 import './page-not-found.scss'
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <section className='page-not-found'>
+      <div className='container page-not-found__container'>
+        <h1 className='page-not-found__title'>404</h1>
+        <SiProbot className='page-not-found__icon' />
+        <p className='page-not-found__subtitle'>
+          The page you were looking for does not exist
+        </p>
+        <Link to='/' className='page-not-found__btn'>
+          Go Home
+        </Link>
+      </div>
+
+    </section>
   )
 }
 
